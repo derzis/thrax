8ccbc4960a4bd369e1640846022b2e986383ecc8
derzis <16887519+derzis@users.noreply.github.com>
2023-06-27 02:26:26 +0200

    Update okru.py

--- okru.py
+++ okru.py
@@ -4,56 +4,57 @@ from selenium.webdriver.common.keys import Keys
 import time
 import os
 
-#instalare si configurare chromedriver
+# Instalare și configurare chromedriver
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
-browser = webdriver.Chrome('chromedriver',chrome_options=chrome_options)
+browser = webdriver.Chrome(options=chrome_options)
 
-#open the web page
-url = "https://ok.ru/video/c5317365" #trebuie editat url-ul canalului
-#browser = webdriver.Chrome()
+p = 'plugin://plugin.video.sendtokodi/?'
+
+# Deschide pagina web
+url = "https://ok.ru/video/c5317365"  # Trebuie editat URL-ul canalului
 browser.get(url)
 
-#scroll down the page to load all the videos
-#scroll down 1000 pixels each time
+# Derulează în jos pentru a încărca toate videoclipurile
+# Derulează cu 1000 de pixeli de fiecare dată
 SCROLL_PAUSE_TIME = 0.5
 
-# Get scroll height
+# Obține înălțimea derulării
 last_height = browser.execute_script("return document.body.scrollHeight")
 
 while True:
-    # Scroll down to bottom
+    # Derulează până jos
     browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
-    # Wait to load page
+    # Așteaptă încărcarea paginii
     time.sleep(SCROLL_PAUSE_TIME)
 
-    # Calculate new scroll height and compare with last scroll height
+    # Calculează noua înălțime a derulării și compar-o cu ultima înălțime
     new_height = browser.execute_script("return document.body.scrollHeight")
     if new_height == last_height:
         break
     last_height = new_height
 
-#get the links and titles
-links = browser.find_elements(By.XPATH, "//div[starts-with(@id, 'video-card_img_')]//a[@aria-label='Vizualizează']")
-titles = browser.find_elements(By.XPATH,"//a[contains(@title, 'Doctor Quinn')]") #trebuie editat cu numele canalului
+# Obține link-urile și titlurile
+links = browser.find_elements(By.XPATH, "//div[@data-l='t,movie']//a[@tabindex='-1']")
+titles = browser.find_elements(By.XPATH, "//a[contains(@title, 'Doctor Quinn')]")  # Trebuie editat cu numele canalului
 
-#create the folder
-folder_name = 'Doctor Quinn' #trebuie editat cu numele canalului
+# Creează folder-ul
+folder_name = 'Doctor Quinn'  # Trebuie editat cu numele canalului
 if not os.path.exists(folder_name):
     os.makedirs(folder_name)
 
-#create .strm file for each video
+# Creează fișierul .strm pentru fiecare videoclip
 for i in range(len(links)):
     link = links[i].get_attribute("href").split('?')[0]
-    #title = titles[i].text
+    #print(link)
     title = titles[i].get_attribute('title')
     file_name = title.replace("/", " ") + ".strm"
     file_path = os.path.join(folder_name, file_name)
     with open(file_path, "w") as file:
-        file.write(link)
+        file.write(p+link)
 
-#close the browser
-browser.close()
+# Închide browser-ul
+browser.quit()
